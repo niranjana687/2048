6efb12a5f939c6a315109b3f2756e5f95bd28946
niranjana687 <niranjana687@gmail.com>
2021-10-05 11:33:00 +0530

    initial commit

---
+++ src/game2048/Tile.java
@@ -0,0 +1,30 @@
+package game2048;
+import java.awt.Color;
+
+public class Tile {
+	int value;
+
+    Color tileColor;
+	//constructor
+	
+	//basic tile
+	public Tile() {
+		value = 0;
+	}
+	
+	//tile with a number
+	public Tile(int number) {
+		value = number;
+	}
+	
+	//methods
+	//getter function to get retrieve tile value.
+	public int getValue() {
+		
+	}
+	
+	//set tile value - when two tiles are being added
+	public void setValue(int value) {
+		
+	}
+}
